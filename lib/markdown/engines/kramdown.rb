15540d53a40901c69f24984dc99ebfb19c20f6f3
Brandon Montgomery <bmonty@gmail.com>
2012-08-17 09:09:50 -0400

    fix code spacing

--- lib/markdown/engines/kramdown.rb
+++ lib/markdown/engines/kramdown.rb
@@ -6,12 +6,12 @@ module Markdown
       h = {}
       
       # todo: find an easier (more generic?) way to setup hash - possible?
-      h[ :auto_ids    ]   = options.fetch( 'auto_ids', nil )      if options.fetch( 'auto_ids',      nil )
+      h[ :auto_ids    ]   = options.fetch( 'auto_ids', nil )     if options.fetch( 'auto_ids',      nil )
       h[ :footnote_nr ]   = options.fetch( 'footnote_nr',nil )   if options.fetch( 'footnote_nr',   nil )
       h[ :entity_output ] = options.fetch( 'entity_output',nil ) if options.fetch( 'entity_output', nil )
       h[ :toc_levels ]    = options.fetch( 'toc_levels',nil )    if options.fetch( 'toc_levels',    nil )
       h[ :smart_quotes ]  = options.fetch( 'smart_quotes',nil )  if options.fetch( 'smart_quotes',  nil )
-      h[ :banner ] = options.fetch( 'banner', true)              if options.fetch( 'banner', nil)
+      h[ :banner ] =        options.fetch( 'banner', true)       if options.fetch( 'banner', nil)
 
       puts "  Converting Markdown-text (#{@content.length} bytes) to HTML using library kramdown (#{Kramdown::VERSION})"
       puts "  using options:"
